9726719ef379cb6c0ce8f6901b64c33e14db9e95
hyun <jhyun_yi@naver.com>
2022-02-22 20:30:14 +0900

    스프링 핵심 원리 - 기본펀  - 객체지향원리적용, 스프링 컨테이너와 스프링빈, 싱글톤 컨테이너, 컴포넌트 스캔

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,45 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderService -> new MemoryMemberRepository()
+    //둘이 객체가 두개 생길까? - 스프링 컨테이너가 어떻게 해결할까?
+
+    @Bean
+    public MemberService memberService(){
+        System.out.println("AppConfig.memberService");
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        System.out.println("AppConfig.memberRepository");
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public OrderService orderService(){
+        System.out.println("AppConfig.orderService");
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy(){
+//        return new FixDiscountPolicy();
+        return  new RateDiscountPolicy();
+    }
+}

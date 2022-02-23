5e97175c7da04da1326fbb86f803d69635a8caeb
hyun <jhyun_yi@naver.com>
2022-02-23 11:49:24 +0900

    롬복 적용 @Getter, @Setter, @RequiredArgsConstructor

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,21 +4,17 @@ import com.sun.source.tree.UsesTree;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
     //생성자 주입을 선택하면 final을 설정할 수 있어서 중간에 값이 바뀌는걸 막을 수 있다
     //생성자나 초기에 값이 안들어오면 컴파일 오류를 내 NullPointerException 방지할수 있다
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
